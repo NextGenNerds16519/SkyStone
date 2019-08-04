93dcad1be14c3e93daf0f46a68f3aefdcd814221
Kaushik Siruvuri <siruvuri.kaushik@gmail.com>
2019-08-04 11:25:23 -0400

    Converted block-code to JAVA. Testing still required.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DynamicControlUpdated.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DynamicControlUpdated.java
@@ -25,6 +25,59 @@ public class DynamicControlUpdated extends OpMode {
         topLeft = gamepad1.left_trigger - gamepad1.right_trigger;
         topRight = gamepad1.left_trigger - gamepad1.right_trigger;
         bottomRight = gamepad1.left_trigger - gamepad1.right_trigger;
+        if (gamepad1.right_trigger == 0 || gamepad1.left_trigger == 0) {
+            if (gamepad1.left_stick_x > 0) {
+                topRight += gamepad1.left_stick_x;
+                bottomRight += gamepad1.left_stick_x;
+            }
+            if (gamepad1.left_stick_x < 0) {
+                topLeft += -gamepad1.left_stick_x;
+                bottomLeft += -gamepad1.left_stick_x;
+            }
+        }
+        if (gamepad1.right_trigger == 0 || gamepad1.left_trigger != 0) {
+            if (gamepad1.left_stick_x > 0) {
+                topRight += gamepad1.left_stick_x;
+                bottomRight += gamepad1.left_stick_x;
+                topLeft += -gamepad1.left_stick_x;
+                bottomLeft += -gamepad1.left_stick_x;
+            }
+            if (gamepad1.left_stick_x < 0) {
+                topLeft += -gamepad1.left_stick_x;
+                bottomLeft += -gamepad1.left_stick_x;
+                topRight += gamepad1.left_stick_x;
+                bottomRight += gamepad1.left_stick_x;
+            }
+        }
+        if (gamepad1.right_trigger == 0 && gamepad1.left_trigger == 0 && gamepad1.left_stick_x == 0) {
+            if (gamepad1.left_bumper) {
+                robot.strafeLeft(1);
+            }
+            if (gamepad1.right_bumper) {
+               robot.strafeRight(1);
+            }
+        }
+        if (gamepad1.left_trigger != 0 && gamepad1.left_bumper) {
+            robot.strafeDiagonalLL(1);
+        }
+        if (gamepad1.right_trigger != 0 && gamepad1.right_bumper) {
+            robot.strafeDiagonalRR(1);
+        }
+        if (gamepad1.left_trigger != 0 && gamepad1.right_bumper) {
+            robot.strafeDiagonalLR(1);
+        }
+        if (gamepad1.right_trigger != 0 && gamepad1.left_bumper) {
+            robot.strafeDiagonalRL(1);
+        }
+        robot.topRightDrive.setPower(topRight);
+        robot.bottomRightDrive.setPower(bottomRight);
+        robot.topLeftDrive.setPower(topLeft);
+        robot.bottomLeftDrive.setPower(bottomLeft);
+        telemetry.addData("Top Left Pow", robot.topLeftDrive.getPower());
+        telemetry.addData("Bottom Left Pow", robot.bottomLeftDrive.getPower());
+        telemetry.addData("Top Right Pow", robot.topRightDrive.getPower());
+        telemetry.addData("Bottom Right Pow", robot.bottomRightDrive.getPower());
+        telemetry.update();
     }
 
 }
