decf65832206b523d310ef8bddc8332079cca324
Kaushik Siruvuri <siruvuri.kaushik@gmail.com>
2019-08-04 07:16:02 -0400

    Some basic code to start on. I'm working on transferring all of our data.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DynamicControlUpdated.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DynamicControlUpdated.java
@@ -1,4 +1,30 @@
 package org.firstinspires.ftc.teamcode;
 
-public class DynamicControlUpdated {
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp(name = "DynamicControlUpdated", group = "")
+public class DynamicControlUpdated extends OpMode {
+
+    HardwareStraferBasic robot = new HardwareStraferBasic(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+    double topLeft = 0;
+    double topRight = 0;
+    double bottomLeft = 0;
+    double bottomRight = 0;
+
+    @Override
+    public void init() {
+        robot.init(hardwareMap);
+    }
+
+    @Override
+    public void loop() {
+        bottomLeft = gamepad1.left_trigger - gamepad1.right_trigger;
+        topLeft = gamepad1.left_trigger - gamepad1.right_trigger;
+        topRight = gamepad1.left_trigger - gamepad1.right_trigger;
+        bottomRight = gamepad1.left_trigger - gamepad1.right_trigger;
+    }
+
 }
