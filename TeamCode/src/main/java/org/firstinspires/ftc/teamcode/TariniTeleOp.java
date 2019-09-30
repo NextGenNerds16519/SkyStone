db206347c65b998e504f9179b428644c172a169c
Neil Agrawal <neil.agrawal6@gmail.com>
2019-09-30 12:56:30 -0400

    Tarini TeleOp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TariniTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TariniTeleOp.java
@@ -1,4 +1,25 @@
 package org.firstinspires.ftc.teamcode;
 
-public class TariniTeleOp {
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp(name = "TariniTeleOp", group = "TeamPractice")
+public class TariniTeleOp extends OpMode {
+
+    HardwareStrafer robot = new HardwareStrafer(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+
+    @Override
+    public void init() {
+        robot.init(hardwareMap);
+    }
+
+    @Override
+    public void loop() {
+        if (gamepad1.left_bumper && gamepad1.right_bumper){
+                robot.moveForward(34);
+        }
+
+    }
 }
