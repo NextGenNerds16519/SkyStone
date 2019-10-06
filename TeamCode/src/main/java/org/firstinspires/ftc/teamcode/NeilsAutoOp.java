9b498775deb0d28155c89f760d2e9dad966288b1
Neil Agrawal <neil.agrawal6@gmail.com>
2019-10-06 14:38:21 -0400

    fixed the AutoOp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NeilsAutoOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NeilsAutoOp.java
@@ -22,7 +22,7 @@ public class NeilsAutoOp extends OpMode {
     @Override
     public void loop() {
 
-        moveDistance(1, 10);
+        moveDistance(1, 10);''
 
     }
 
@@ -44,6 +44,8 @@ public class NeilsAutoOp extends OpMode {
         robot.bottomRightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         robot.topLeftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         robot.topRightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        while(robot.bottomLeftDrive.isBusy() && robot.bottomRightDrive.isBusy() && robot.topLeftDrive.isBusy() && robot.topRightDrive.isBusy())
     }
     public void move(float power) {
 
